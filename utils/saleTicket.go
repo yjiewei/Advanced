66be81d49b7680d57c8dcdd665f977b738990a7e
yangjiewei <yang_7131@163.com>
2022-11-30 23:09:17 +0800

    feat:上锁，加一把大锁

--- utils/saleTicket.go
+++ utils/saleTicket.go
@@ -7,12 +7,13 @@ import (
 	"time"
 )
 
-// 并发编程中的共享资源安全问题
+// fixme 加了锁之后，原来用的等待组用起来不对。导致不能正常加锁？
+// 并发编程中的共享资源安全问题 https://pkg.go.dev/sync#Mutex
 var ticket = 10
 var wg sync.WaitGroup
 
 func Sale() {
-	wg.Add(4) // 如果数量有多余，会出现死锁问题
+	//wg.Add(4) // 如果数量有多余，会出现死锁问题
 	// 模拟4个售票口，即4个goroutine
 	go tickets("售票口1")
 	go tickets("售票口2")
@@ -20,18 +21,23 @@ func Sale() {
 	go tickets("售票口4")
 
 	// 防止主线程执行完直接结束了，协程还没开始的情况
-	// time.Sleep(2 * time.Second)
-	// 用同步等待的方法取代上面的线程睡眠，但是并没有解决共享变量的问题
-	fmt.Println("如果先输出这行，说明主线程已经执行完了，我们要等等。")
-	wg.Wait()
-	fmt.Println("协程也执行完毕了，那就放行吧。")
+	time.Sleep(2 * time.Second)
+
+	// 用同步等待的方法取代上面的线程睡眠，但是并没有解决共享变量的问题，上锁之后再用这个，锁没有生效
+	/*	fmt.Println("如果先输出这行，说明主线程已经执行完了，我们要等等。")
+		wg.Wait()
+		fmt.Println("协程也执行完毕了，那就放行吧。")*/
 
 }
 
 func tickets(name string) {
 	rand.Seed(time.Now().UnixNano())
+	// 原语级别的 整了一把锁
+	mutex := sync.Mutex{}
 	for {
 		// 线程进入之后条件可能会改变，那输出可能就不对了，是否考虑加锁？go有其他方式处理吗
+		// 用mutex.Lock()，但是这把锁要加在哪里？如何分析？ticket>0，到ticket--，这里可能被其他线程占用了，所以要在这里加锁
+		mutex.Lock()
 		if ticket > 0 {
 			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 			fmt.Println(name, "售出电影票：", ticket)
@@ -40,8 +46,9 @@ func tickets(name string) {
 			fmt.Println(name, "已售罄，没有电影票了。")
 			break
 		}
+		mutex.Unlock()
 	}
-	wg.Done()
+	//wg.Done()
 }
 
 /**
