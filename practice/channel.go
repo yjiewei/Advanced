538b1c17f12b7c4fb71e09eaec41d15b19184170
yangjiewei <yang_7131@163.com>
2022-12-02 22:02:01 +0800

    feat:关闭通道

--- practice/channel.go
+++ practice/channel.go
@@ -18,16 +18,36 @@ func main() {
 	//fmt.Println("主协程执行结束...")
 
 	// 2.利用通道进行通信
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		fmt.Println("执行子协程中的循环：", i)
+	//	}
+	//	// 给通道写入数据给主协程去读
+	//	channel <- 1
+	//	fmt.Println("子协程执行结束...")
+	//}()
+	//// 读不到数据就会阻塞，所以会等在这里
+	//data := <-channel
+	//fmt.Println("从通道中读取到的数据是：", data)
+	//fmt.Println("主协程执行结束...")
+
+	// 3.通道通信还要关闭通道
 	go func() {
 		for i := 0; i < 10; i++ {
-			fmt.Println("执行子协程中的循环：", i)
+			// 给通道写入数据给主协程去读
+			channel <- i
+			//fmt.Println("执行子协程中的循环：", i)
 		}
-		// 给通道写入数据给主协程去读
-		channel <- 1
 		fmt.Println("子协程执行结束...")
+		close(channel)
 	}()
 	// 读不到数据就会阻塞，所以会等在这里
-	data := <-channel
-	fmt.Println("从通道中读取到的数据是：", data)
+	for {
+		data, ok := <-channel
+		fmt.Println("从通道中读取到的数据是：", data, ok)
+		if !ok {
+			break
+		}
+	}
 	fmt.Println("主协程执行结束...")
 }
