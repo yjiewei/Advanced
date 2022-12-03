4a26c67dde89dc9037dd731b71c13bd1878f5deb
yangjiewei <yang_7131@163.com>
2022-12-03 17:26:20 +0800

    feat:缓冲通道、定向通道

--- practice/channel.go
+++ practice/channel.go
@@ -32,15 +32,15 @@ func main() {
 	//fmt.Println("主协程执行结束...")
 
 	// 3.通道通信还要关闭通道
-	go func() {
-		for i := 0; i < 10; i++ {
-			// 给通道写入数据给主协程去读
-			channel <- i
-			//fmt.Println("执行子协程中的循环：", i)
-		}
-		fmt.Println("子协程执行结束...")
-		close(channel)
-	}()
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		// 给通道写入数据给主协程去读
+	//		channel <- i
+	//		//fmt.Println("执行子协程中的循环：", i)
+	//	}
+	//	fmt.Println("子协程执行结束...")
+	//	close(channel)
+	//}()
 	// 读不到数据就会阻塞，所以会等在这里
 	//for {
 	//	data, ok := <-channel
@@ -50,9 +50,45 @@ func main() {
 	//	}
 	//}
 
-	// 4.范围循环
-	for v := range channel {
-		fmt.Println("从通道中读取到的数据是：", v)
-	}
+	//// 4.范围循环
+	//for v := range channel {
+	//	fmt.Println("从通道中读取到的数据是：", v)
+	//}
+	//fmt.Println("主协程执行结束...")
+
+	// 5.缓冲通道 声明通道的时候同时声明capacity
+	//cacheChannel := make(chan int, 5)
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		cacheChannel <- i
+	//		fmt.Println("往子协程写入数据：", i)
+	//	}
+	//	fmt.Println("子协程写入数据结束")
+	//	// 要关通道，不然主协程会等着读，导致死锁
+	//	close(cacheChannel)
+	//}()
+	//
+	//for v := range cacheChannel {
+	//	fmt.Println("从通道中读取到的数据是：", v)
+	//}
+	//fmt.Println("主协程执行结束...")
+
+	// 6.定向通道 也就是单向，其实这个不在声明的时候使用，没有管道是只读或者只写的
+	// 所以他通常用在函数声明上，限制函数对通道的操作，实现职责分明
+	directChannel := make(chan int)
+	go directChannelWrite(directChannel)
+	directValue := <-directChannel
+	fmt.Println("从通道中读取到的数据是：", directValue)
 	fmt.Println("主协程执行结束...")
+
+}
+
+func directChannelWrite(directChannel chan<- int) {
+
+	directChannel <- 1
+	fmt.Println("往子协程写入数据：", 1)
+
+	fmt.Println("子协程写入数据结束")
+	// 要关通道，不然主协程会等着读，导致死锁
+	close(directChannel)
 }
